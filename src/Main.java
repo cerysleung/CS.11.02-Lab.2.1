f52bf306801fde01347fca64234baaea228d63b2
cerysleung <cerysl2027@student.cis.edu.hk>
2025-03-26 19:57:07 +0800

    lab 2.1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Cerys
 *****************************************************************************************************
 */
 
@@ -13,19 +13,42 @@ public class Main {
     }
 
     // 1. add
-    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
     // 2. add
+    public static int add(int a, int b, int c, int d) {
+        return add(a,b) + add (c,d);
+    }
 
     // 3. morningGreeting
+    public static String morningGreeting(String name) {
+        return "早上好, " + name + "!";
+    }
 
     // 4. afternoonGreeting
+    public static String afternoonGreeting(String name) {
+        return "下午好, " + name + "!";
+    }
 
     // 5. triple
+    public static String triple(String name) {
+        return name + name + name;
+    }
 
     // 6. half
+    public static double half(int a) {
+        return a / 2.0;
+    }
 
     // 7. roundPositiveValueToNearestInteger
+    public static int roundPositiveValueToNearestInteger(double a) {
+        return (int) (a + 0.5);
+    }
 
     // 8. roundNegativeValueToNearestInteger
-
+    public static int roundNegativeValueToNearestInteger(double a) {
+        return (int) (a -0.5);
+    }
 }
